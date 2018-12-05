3bca1e01fa0af086dc4ab3a8d0cbff6630e52595
jbeullens <jeroen.beullens@persgroep.be>
2018-12-05 11:08:26 +0100

    day5 input and input parsing

---
+++ src/main/java/be/upgrade/it/adventofcode2018/day5/AlchemicalReduction.java
@@ -0,0 +1,12 @@
+package be.upgrade.it.adventofcode2018.day5;
+
+import be.upgrade.it.adventofcode2018.CommonUtils;
+
+public class AlchemicalReduction {
+    public static final String INPUT = "day5/input.txt";
+    public static void main(String[] args) {
+        String input = CommonUtils.resourceToStringList(INPUT)[0];
+        System.out.println(input);
+
+    }
+}
